3abc7b9eda1980c592869e6ee77dec6b9a68e8a2
yuki-create <yuukiyamanaka0813@gmail.com>
2019-11-25 19:56:02 +0900

    ロジスティック写像のlyapunov exponentを求めるテストができた

--- chaos/logistic.c
+++ chaos/logistic.c
@@ -5,13 +5,16 @@ double initial_d = 0.0;
 double sum = 0.0;
 double accurate_le = 0.0;
 double sum_log = 0.0;
-void updateLyapunovExponent(int time_steps, double x, double x_d);
-void accurateLyapunovExponent(int time_steps, double a, double x);
+double a = 4.0; // chaos
+double x=0.01;
+double x_d=0.0100000001;
+double sum_d = 0.0;
+
+void updateLyapunovExponent(int time_steps);
+void accurateLyapunovExponent(int time_steps);
 
 int main(void){
-  double a = 4.0; // chaos
-  double x=0.01;
-  double x_d=0.010000000001;
+
   int n=0;
 
 //  initial_d = sqrt( pow( (x-x_d), 2.0 ) );
@@ -20,24 +23,31 @@ int main(void){
     x=a*x*(1.0-x);
     x_d=a*x_d*(1.0-x_d);
     //printf("%d %f %f\n",n,x,x_d);
-    updateLyapunovExponent(n,x,x_d);
-    accurateLyapunovExponent(n,a,x);
+    updateLyapunovExponent(n);
+    accurateLyapunovExponent(n);
     printf("%d %f %f\n",n, lyapunov, accurate_le);
   }
   return 0;
 }
 
 /* 2つの出力の差　引数(l,l_d) */
-void updateLyapunovExponent(int time_steps, double x, double x_d){
+ /*void updateLyapunovExponent(int time_steps, double x, double x_d){
   double norm2=0.0;
-  norm2 = sqrt( pow( (x-x_d), 2.0 ) );
-  sum += log(norm2/initial_d);
+  norm2 =  fabs(x-x_d);
 //  sum += log(norm2/initial_d);
-  lyapunov = sum / time_steps;
+  lyapunov = log(norm2/initial_d) / time_steps;
+} */
+void updateLyapunovExponent(int time_steps){
+  double norm2=0.0;
+  norm2 =  fabs(x-x_d); // gamma_k
+//  sum += log(norm2/initial_d);
+  x_d = x + (initial_d/norm2)*(x_d-x);
+  sum_d = sum_d + log(norm2/initial_d);
+  lyapunov = sum_d / time_steps;
 }
 
 // f'(x) = -2ax+a
-void accurateLyapunovExponent(int time_steps, double a, double x){
+void accurateLyapunovExponent(int time_steps){
   sum_log += log( fabs(-2.0*a*x+a) );
   accurate_le = sum_log/time_steps;
 }
